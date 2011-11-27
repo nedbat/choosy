a3d5b8e6bf454d4dace49e84452a1642f67c5950
Ned Batchelder <ned@nedbatchelder.com>
2011-11-27 17:25:37 -0500

    Use CodeMirror

--- server/settings.py
+++ server/settings.py
@@ -1,6 +1,8 @@
 # Django settings for server project.
 import os
 
+HERE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -57,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'srvstat'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -73,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(HERE_DIR, "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -107,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.abspath(os.path.dirname(__file__)),
+    HERE_DIR,
 )
 
 INSTALLED_APPS = (
