91e6dcc369ab6f86705c657928b797cf2f404bd3
Ned Batchelder <ned@nedbatchelder.com>
2011-12-11 00:04:38 -0500

    A common base template, and the beginnings of a style(?)

--- server/desk/urls.py
+++ server/desk/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('desk.views',
-    url(r'^$', 'index'),
+    url(r'^$', 'index', name='desk'),
     url(r'^new/$', 'edit', name='new_exercise'),
     url(r'^(?P<exid>\d+)/edit/$', 'edit', name='edit_exercise'),
 )
--- server/gym/urls.py
+++ server/gym/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('gym.views',
-    url(r'^$', 'index'),
+    url(r'^$', 'index', name='gym'),
     url(r'^run/(?P<exid>\d+)/$', 'run'),
     url(r'^(?P<exid>\d+)/(?P<slug>.*)$', 'exercise', name='show_exercise'),
 )
