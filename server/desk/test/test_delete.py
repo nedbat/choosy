5cdbe5c6e74ad576b167f8a846bb2ee339672a18
Ned Batchelder <ned@nedbatchelder.com>
2011-12-30 21:38:41 -0500

    Hand-edit the yaml fixture so I can make more.

--- server/desk/test/test_delete.py
+++ server/desk/test/test_delete.py
@@ -10,12 +10,12 @@ class DeleteTest(ChoosyDjangoTestCase):
     fixtures = ['basic.yaml']
 
     def test_deleting_exercise(self):
-        self.assertQuerysetEqual(Exercise.objects.all(), ['<Exercise: Variables>', '<Exercise: Functions>', '<Exercise: Lists>'])
-        ex = Exercise.objects.get(slug="functions")
+        self.assertQuerysetEqual(Exercise.objects.all(), ['<Exercise: Variables>', '<Exercise: Lists>', '<Exercise: Functions>'])
+        ex = Exercise.objects.get(slug="lists")
         client = Client()
         response = client.post(reverse("delete_exercise", args=[ex.id]), {})
         self.assertEqual(response.status_code, 200)
-        self.assertQuerysetEqual(Exercise.objects.all(), ['<Exercise: Variables>', '<Exercise: Lists>'])
+        self.assertQuerysetEqual(Exercise.objects.all(), ['<Exercise: Variables>', '<Exercise: Functions>'])
 
     def test_deleting_requires_post(self):
         ex = Exercise.objects.get(slug="functions")
