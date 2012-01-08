9a6643768fd2288df8e8df7ae7937f41b5627a8f
Ned Batchelder <ned@nedbatchelder.com>
2012-01-08 16:20:05 -0500

    Move most of the production settings into prod_settings.py

--- server/prod_settings.py
+++ server/prod_settings.py
@@ -1,4 +1,17 @@
+# Production settings.
 from settings import *
 
+# Hmm, this is what sucks about the --settings=prod_settings.py way of doing
+# things: can't affect something in settings.py directly.
 INSTALLED_APPS = [a for a in INSTALLED_APPS if a not in ['django_nose']]
 
+DEBUG = False
+
+STATIC_ROOT = '/home/nedbat/webapps/choosy_static/'
+STATIC_URL = 'http://choosepython/static/'
+
+EMAIL_HOST = 'smtp.webfaction.com'
+EMAIL_HOST_USER = 'choosy'
+EMAIL_HOST_PASSWORD = 'choosy_mailbox'
+DEFAULT_FROM_EMAIL = 'choosy@choosepython.com'
+SERVER_EMAIL = 'choosy@choosepython.com'
